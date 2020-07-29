699f2b0cb0994f973a8e6a729256a40eeb0baebb
David Manso <david.manso@tadaima.studio>
2020-07-29 20:29:35 +0200

    Basic scaffold refactor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,9 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang='pug'>
+  div
+    p hello
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
